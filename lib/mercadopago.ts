577cec9f601d4073cd3dde8723c96ffebc3a8e24
DSBENI2025 <santandeibson9@gmail.com>
2026-09-19 22:21:15 -0300

    feat: add lib/mercadopago.ts

---
+++ lib/mercadopago.ts
@@ -0,0 +1,21 @@
+import {
+  MercadoPagoConfig,
+  PreApproval,
+  WebhookSignatureValidator,
+} from "mercadopago";
+
+export function getMercadoPagoClient() {
+  const accessToken = process.env.MERCADO_PAGO_ACCESS_TOKEN;
+
+  if (!accessToken) {
+    throw new Error("MERCADO_PAGO_ACCESS_TOKEN não configurado.");
+  }
+
+  return new MercadoPagoConfig({ accessToken });
+}
+
+export function getPreApprovalClient() {
+  return new PreApproval(getMercadoPagoClient());
+}
+
+export { WebhookSignatureValidator };
